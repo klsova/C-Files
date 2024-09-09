f6645439a3297af33c26ceeea79bd61a48df4152
klsova <kane.sova@gmail.com>
2024-09-09 20:55:45 +0300

    Banaani on hyvää :DD

---
+++ EscapeSequence.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Tykkään\n banaanista\n");
+    return 0;
+}
---
+++ HelloWorld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello World!\n");
+    printf("Banaani on hyvää :DD\n");
+
+    // Tältä näyttää kommentti C:ssä
+    // Banaani on hyvää :DDD
+
+    /*
+    Tältä näyttää useemman rivin kommentti C:ssä
+    Banaani on muute pirum hyvveee :-DDDD
+    */
+
+    return 0;
+}
